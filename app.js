d631c3162ea7df5cbdd1a5c33d6b7d7c30145917
Daria Balina <59246828+CodigoPensador92@users.noreply.github.com>
2022-07-15 09:01:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+import PortalVue from 'portal-vue'
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Импортируйте файлы CSS Bootstrap и BootstrapVue (порядок важен)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Сделайте BootstrapVue доступным для всего проекта
+Vue.use(BootstrapVue)
+// При желании установите плагин компонентов иконок BootstrapVue
+Vue.use(IconsPlugin)
+
 
 const app = Vue.createApp({
     el: '#demo',
